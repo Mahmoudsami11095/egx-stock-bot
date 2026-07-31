5431df421387a5b97c822c8fdb547c0f6152b06d
Mahmoudsami11095 <37407727+Mahmoudsami11095@users.noreply.github.com>
2026-07-31 03:36:15 +0300

    feat(deps): upgrade all frontend packages to match FitnessApp reference (Angular 22, PrimeNG 21, PrimeUIX themes 2)

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
-import Aura from '@primeng/themes/aura';
+import Aura from '@primeuix/themes/aura';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
